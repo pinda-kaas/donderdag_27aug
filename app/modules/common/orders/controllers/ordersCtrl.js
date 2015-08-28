aed46d846a072a1b228a7c53e1af5ca821958322
pindakaas <dingen2008@gmail.com>
2015-08-29 09:22:27 +1000

    changed autocomplete to bs3

--- app/modules/common/orders/controllers/ordersCtrl.js
+++ app/modules/common/orders/controllers/ordersCtrl.js
@@ -1,35 +1,49 @@
-app.controller('OrdersCtrl', function ($scope, $filter, $location, configService, orderData, settlementsData, completeOrders,$state) {
+app.controller('OrdersCtrl', function ($scope, $filter, $location, configService, orderData, settlementsData, completeOrders, $state,$http) {
 
-  $scope.ordersTabActive = true;
-  $scope.settlementsTabActive = false;
-  $scope.completedTabActive = false;
+    $scope.ordersTabActive = true;
+    $scope.settlementsTabActive = false;
+    $scope.completedTabActive = false;
 
-  console.log('ordersctr2cbvsl startesd');
+    console.log('ordersctr2cbvsl startesd');
 
-  if (orderData.length == 0) {
-    $scope.ordersTabActive = false;
-    $scope.settlementsTabActive = true;
-  }
-  else {
-    $scope.displayedRows = orderData;
-    $scope.accounts = orderData;
-  }
+    if (orderData.length == 0) {
+        $scope.ordersTabActive = false;
+        $scope.settlementsTabActive = true;
+    }
+    else {
+        $scope.displayedRows = orderData;
+        $scope.accounts = orderData;
+    }
 
-  $scope.settlements = settlementsData;
-  $scope.completeOrders = completeOrders;
+    $scope.settlements = settlementsData;
+    $scope.completeOrders = completeOrders;
 
-  console.log($scope.accounts);
+    console.log($scope.accounts);
 
-  $scope.selectAdviser = function (adviser) {
-    $scope.adviser = adviser;
-  }
+    $scope.selectAdviser = function (adviser) {
+        $scope.adviser = adviser;
+    }
 
-  $scope.selectAccount = function () {
+    //$scope.selectAccount = function () {
+    //
+    //  $state.go('orderDetail', {myParam: {some: 'thing'}})
+    //
+    //
+    //}
+    //
 
-    $state.go('orderDetail', {myParam: {some: 'thing'}})
-
-
-  }
+    $scope.getLocation = function (val) {
+        return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                address: val,
+                sensor: false
+            }
+        }).then(function (response) {
+            return response.data.results.map(function (item) {
+                return item.formatted_address;
+            });
+        });
+    };
 
 
 });
