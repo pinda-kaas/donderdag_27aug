963a76c629343d386ec2fc72cfd1b46d01359838
Dimitri Couwenberg <Dimitri.Couwenberg@macquarie.com>
2015-09-02 10:56:27 +1000

    fixed advisercombo

--- app/modules/common/orders/controllers/ordersCtrl.js
+++ app/modules/common/orders/controllers/ordersCtrl.js
@@ -18,41 +18,22 @@ app.controller('OrdersCtrl', function ($scope, $filter, $location, configService
   $scope.settlements = settlementsData;
   $scope.completeOrders = completeOrders;
 
-  ///console.log($scope.accounts);
-
-  $scope.selectAdviser = function (adviser) {
-    $scope.adviser = adviser;
-  }
-
-  //$scope.getAccount = function (val) {
-  //  return $http.get('/modules/common/orders/mock/ordersTabMockData.json', {
-  //    params: {
-  //      accountName: val
-  //    }
-  //  })
-  //    .then(function (response) {
-  //      return response.data.map(function (item) {
-  //        //console.log (item.account.accountName);
-  //        return item.account.accountId.key;//+ "-"+ item.account.accountName;
-  //      });
-  //    });
-  //};
+  $scope.advisers = ['Mark', 'Ed', 'Fred',''];
 
   $scope.onSelect = function ($item, $model, $label) {
     $scope.$item = $item;
     $scope.$model = $model;
     $scope.$label = $label;
 
-    console.log('id=',$item);
+    console.log('id=', $item);
 
     $state.go('orderDetail', {myParam: $item.account.accountId.key});
   };
 
 
-
-  $scope.resetFilter= function(){
-    console.log('reset filter',$scope.assetType);
-    $scope.assetType ='All';
+  $scope.resetFilter = function () {
+    console.log('reset filter', $scope.assetType);
+    $scope.assetType = 'All';
   }
 
 
